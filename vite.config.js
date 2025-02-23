8790ec1ee6059f497f5617f1b5e4ac618a1627a5
VinuBhau <roddvinayak98@gmail.com>
2025-02-23 15:54:20 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,10 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
 
-  server:{
-    host: '0.0.0.0',
+  server: {
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: process.env.PORT || 3000,  // Use Render's provided port or default to 3000
   }
+  
 
 })
