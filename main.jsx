3c20236d64e2f6b6fcdcb03f68440436a9d59127
VinuBhau <roddvinayak98@gmail.com>
2025-02-25 22:33:07 +0530

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { Provider } from 'react-redux'; // Import Provider from react-redux
-import store from './redux/stores.js'; // Import your Redux store from the store.js
+import store from './redux/stores'; // Import your Redux store from the store.js
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
